98dce16afa28a81cfaa87e44d9d07340d47b19a0
Kevin Alexander <kevin.aw2204@gmail.com>
2020-05-11 10:28:49 +0700

    Perbaiki ssl postgre heroku

--- index.js
+++ index.js
@@ -9,16 +9,14 @@ const { Client } = require('pg');
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  ssl: {rejectUnauthorized: false},
 });
 
 client.connect();
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+client.query('SELECT * FROM usertable', (err, res) => {
   if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
+  else console.log(res.rows);
   client.end();
 });
 
