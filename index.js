29e387b95e9b123ccbccd2e370db9b333b55dff5
matthew178 <64057050+matthew178@users.noreply.github.com>
2020-05-12 15:32:26 +0700

    Tambah endpoint delete user

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const app = express();
 const user = require('./routes/user');
-const review = require('./routes/review');
-const collection = require('./routes/collection');
-const resto = require('./routes/resto');
+// const review = require('./routes/review');
+// const collection = require('./routes/collection');
+// const resto = require('./routes/resto');
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("uploads"));
 app.use('/api/user', user);
-app.use('/api/review', review);
-app.use('/api/collec', collection);
-app.use('/api/resto', resto);
+// app.use('/api/review', review);
+// app.use('/api/collec', collection);
+// app.use('/api/resto', resto);
 
 require('dotenv').config();
 
 const { Client } = require('pg');
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: "postgres://lfpnnieeczkxuy:1f05bb5f73a4030094852c9a3b7f7b793ad51e88eda77a648598bfc58e4de0a0@ec2-54-81-37-115.compute-1.amazonaws.com:5432/de1fta97koh1rk",
   ssl: {rejectUnauthorized: false},
 });
 
@@ -37,6 +37,6 @@ app.get("/", async function(req, res){
   res.send(result);
 });
 
-app.listen(process.env.PORT, function(){
-    console.log(`listening port ${process.env.PORT}`);
+app.listen(3000, function(){
+    console.log(`listening port 3000`);
 });
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,41 @@
 const express = require("express");
 const router = express.Router();
+require('dotenv').config();
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: "postgres://lfpnnieeczkxuy:1f05bb5f73a4030094852c9a3b7f7b793ad51e88eda77a648598bfc58e4de0a0@ec2-54-81-37-115.compute-1.amazonaws.com:5432/de1fta97koh1rk",
+  ssl: {rejectUnauthorized: false},
+});
+
+client.connect();
+
+function executeQuery(query){
+    return new Promise(function(resolve, reject){
+        client.query(query, (err, res) => {
+        if(err) reject(err);
+        else resolve(res.rows);
+        });
+    });
+}
+
+router.delete('/deleteUser', async function(req, res) {
+    let cari = await executeQuery(`SELECT * FROM usertable WHERE apikey = '${req.query.apiKey}'`);
+    if(cari.length > 0){
+        if(cari[0].tipe == 0){
+            let hapus = await executeQuery(`DELETE FROM usertable WHERE username = '${req.body.username}'`);
+            res.send(hapus);
+        }
+        else{
+            let carilagi = await executeQuery(`SELECT * FROM usertable WHERE apikey = '${req.query.apiKey}' and username = '${req.body.username}'`);
+            if(carilagi.length > 0){
+                let hapus = await executeQuery(`DELETE FROM usertable WHERE username = '${req.body.username}'`);
+                res.send(hapus);
+            }
+            else res.status(401).send("User tidak memiliki hak akses untuk menghapus user lain!");
+        }
+    }else res.status(404).send("User tidak ditemukan!");
+});
 
 module.exports = router;
